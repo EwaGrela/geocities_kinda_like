b620ad8d1eb67dd6337f261d042abf0aca1771fb
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2018-04-04 18:10:54 +0200

    changes

--- change_db.sql
+++ change_db.sql
@@ -5,8 +5,9 @@ ALTER TABLE film ADD category_id INTEGER;
 CREATE VIEW helper as SELECT film_list.category, film_list.title, category.category_id FROM film_list JOIN film ON film_list.title = film.title JOIN category ON film_list.category = category.name;
  -- ustawiamy wartosci
 UPDATE film SET category_id = (SELECT category_id FROM helper WHERE film.title = helper.title);
--- usuwamy z bazy view i nadmiarowa tabele
+-- usuwamy z bazy view 
 DROP VIEW helper;
+-- usuwamy nadmiarową tabelę
 DROP TABLE film_category;
 
 
